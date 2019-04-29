b4cb0450cb0bb1ae4775f38117a92d4954e169e4
ikicapitan <ikicapitan141@gmail.com>
2019-04-28 21:57:51 -0300

    28-04

---
+++ scenes/gui.gd
@@ -0,0 +1,29 @@
+extends CanvasModulate
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_btn_select_button_down():
+	pass # Replace with function body.
+
+func _on_btn_move_button_down():
+	pass # Replace with function body.
+
+func _on_btn_w1_button_down():
+	pass # Replace with function body.
+
+func _on_btn_w2_button_down():
+	pass # Replace with function body.
+
+func _on_btn_cam_button_down():
+	pass # Replace with function body.
+
+
+func _on_btn_exit_button_down():
+	pass # Replace with function body.
+
+
+
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,5 +1,18 @@
 extends Node2D
 
+export (PackedScene) var lvl_1
+export (PackedScene) var lvl_2
+export (PackedScene) var lvl_3
+export (PackedScene) var lvl_4
+export (PackedScene) var lvl_5
+
+export (PackedScene) var gui
+
+
 func _ready():
+	var newlvl = lvl_1.instance()
+	add_child(newlvl)
+	var newgui = gui.instance()
+	add_child(newgui)
 	gamehandler.puntero = $objetivo
 
